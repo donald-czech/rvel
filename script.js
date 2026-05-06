effab25c02e65ff6a7e5b0125df9b06e40026ec8
donald-czech <rvydareny@gmail.com>
2026-05-06 12:25:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,19 @@ document.querySelectorAll(".nav a").forEach(link => {
   });
 });
 
+// ANIMACE SLUŽEB
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.15 });
+
+cards.forEach(card => observer.observe(card));
+
 // LIGHTBOX
 const images = document.querySelectorAll(".gallery img");
 const lightbox = document.getElementById("lightbox");
